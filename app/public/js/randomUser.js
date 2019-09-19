b39e15a7e12997ea8b6ac01ecf4e127ea2180724
Arpita Bali <baliarpita@gmail.com>
2019-09-19 09:03:33 -0400

    Added button that refreshes data using event handling
    Added button that refreshes data using event handling

--- app/public/js/randomUser.js
+++ app/public/js/randomUser.js
@@ -59,6 +59,61 @@ var randomUser = new Vue({
             fetch('https://randomuser.me/api')
                 .then(response => response.json())
                 .then(json => { randomUser.newUser = json.results[0] });
+        },
+        refreshData() {
+            this.fetchUser();
+            this.newUser = {
+                "gender": "",
+                "name": {
+                    "title": "",
+                    "first": "",
+                    "last": ""
+                },
+                "location": {
+                    "street": "",
+                    "city": "",
+                    "state": "",
+                    "postcode": "",
+                    "coordinates": {
+                        "latitude": "",
+                        "longitude": ""
+                    },
+                    "timezone": {
+                        "offset": "",
+                        "description": ""
+                    }
+                },
+                "email": "",
+                "login": {
+                    "uuid": "",
+                    "username": "",
+                    "password": "",
+                    "salt": "",
+                    "md5": "",
+                    "sha1": "",
+                    "sha256": ""
+                },
+                "dob": {
+                    "date": "",
+                    "age": null
+                },
+                "registered": {
+                    "date": "",
+                    "age": null
+                },
+                "phone": "",
+                "cell": "",
+                "id": {
+                    "name": "",
+                    "value": ""
+                },
+                "picture": {
+                    "large": "",
+                    "medium": "",
+                    "thumbnail": ""
+                },
+                "nat": ""
+            };
         }
     },
     created: function () {
